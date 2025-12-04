08ac07fc996255ce3a19216142af7157443e7c44
yashdubey2004 <dubeyyash310@gmail.com>
2025-12-04 08:37:28 +0530

    01

---
+++ first01/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <h1>Yash Dubey</h1>
+  )
+}
+
+export default App
---
+++ first01/src/Yash.jsx
@@ -0,0 +1,7 @@
+function Yash(){
+    return(
+        <h1>Yash Satish Dubey</h1>
+    )
+}
+
+export default Yash
